c4384c3d92fd070bab5ae749775b392bee7d396e
Ayeshahanif12 <ayeshahanif.0317@gmail.com>
2026-09-04 14:50:17 +0500

    fix: redact full value of SUPABASE_SERVICE_ROLE_KEY in .env using wildcard pattern

--- _redact.sh
+++ _redact.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ -f server/.env ]; then
-  sed -i.bak 's/SUPABASE_SERVICE_ROLE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9\.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjYWNmanFrdWlrbHdzanVmcmlpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4ODA4NTM1NiwiZXhwIjoyMTAzNjYxMzU2fQ\.7lFGVD1G1vkBBoOLZGgmSz_WpjBzJuG-VUkF1YWf31I/SUPABASE_SERVICE_ROLE_KEY=REDACTED/' server/.env
+  sed -i.bak 's/SUPABASE_SERVICE_ROLE_KEY=.*/SUPABASE_SERVICE_ROLE_KEY=REDACTED/' server/.env
   rm -f server/.env.bak
 fi
